0a58388acbc8cd6fefbd442a8b28961b218cff0a
ROOOO <ql.wang.gd@gmail.com>
2016-05-09 15:10:02 +0800

    download bug fixed.

--- ytbDL.py
+++ ytbDL.py
@@ -15,6 +15,7 @@ class YtbMP3Local:
           self.port = v
     self.address = ':' + self.port
     self.files = []
+    self.audioPostfix = ['aac', 'vorbis', 'mp3', 'm4a', 'opus', 'wav', 'ogg']
 
   def getHTML(self, url):
     self.address = 'http://' + url + self.address
@@ -25,14 +26,14 @@ class YtbMP3Local:
   def searchLocalExistsAudioFiles(self):
     fileNames = os.listdir(os.path.dirname(os.path.realpath(__file__)))
     for fileName in fileNames:
-      if os.path.splitext(fileName)[1] == '.acc' or os.path.splitext(fileName)[1] == '.vorbis' or os.path.splitext(fileName)[1] == '.mp3' or os.path.splitext(fileName)[1] == '.m4a' or os.path.splitext(fileName)[1] == '.opus' or os.path.splitext(fileName)[1] == '.wav' or os.path.splitext(fileName)[1] == '.ogg':
+      if os.path.splitext(fileName)[1] + '.' in self.audioPostfix:
         self.files.append(fileName)
 
   def getAudioFiles(self):
-    pattern = re.compile(r'<li><a href="(.*?)">(.*?aac|vorbis|mp3|m4a|opus|wav|ogg)</a>')
+    pattern = re.compile(r'<li><a href="(.*?)">(.*?)</a>')
     items = re.findall(pattern, self.html)
     for item in items:
-      if item[1] not in self.files:
+      if re.split(re.compile(r'\.'), item[1])[1] in self.audioPostfix and item[1] not in self.files:
         os.popen('wget ' + self.address + '/' + item[0])
 
 if __name__ == '__main__':
