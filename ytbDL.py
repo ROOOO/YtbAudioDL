f307c6b1e4f5a435e8aae8acb50bb0bd92d9b957
ROOOO <ql.wang.gd@gmail.com>
2016-05-17 09:54:10 +0800

    cache wget files.

--- ytbDL.py
+++ ytbDL.py
@@ -28,23 +28,35 @@ class YtbMP3Local:
     self.html = urllib2.urlopen(self.address).read()
 
   def searchLocalExistsAudioFiles(self):
-    fileNames = os.listdir(os.path.dirname(os.path.realpath(__file__)))
-    for fileName in fileNames:
-      self.files.append(fileName)
+    if self.output == '':
+      fileNames = os.listdir(os.path.dirname(os.path.realpath(__file__)))
+      for fileName in fileNames:
+        self.files.append(fileName)
+    else:
+      fileNames = os.listdir(self.output)
+      for fileName in fileNames:
+        self.files.append(fileName)
 
   def getAudioFiles(self):
     pattern = re.compile(r'<a href="/(.*?)">(.*?)</a>')
     items = re.findall(pattern, self.html)
     l = len(items)
     c = 0
+    if self.output == '':
+      os.popen('rm *.tmp')
+    else:
+      os.popen('rm ' + self.output + '/' + '*.tmp')
+
     for item in items:
       c += 1
       if re.search(re.compile(r'\.'), item[1]) and item[1] != '../' and item[1] not in self.files:
         # os.popen('wget ' + self.address + '/' + item[0])
         if self.output != '':
-          os.popen('wget -c ' + '-P "' + self.output + '" "' + self.prefix + item[0] + '"')
+          os.popen('wget -c ' + '-P "' + self.output + '" "' + self.prefix + item[0] + '.tmp"')
+          os.popen('mv ' + os.path.join(self.output, item[1] + '.tmp ') + os.path.join(self.output, item[1]))
         else:
-          os.popen('wget -c "' + self.prefix + item[0] + '"')
+          os.popen('wget -c "' + self.prefix + item[0] + '.tmp"')
+          os.popen('mv ' + item[1] + '.tmp ' + item[1])
         print 'Downloading ' + item[1] + '========' + str(int(c / float(l) * 100)) + u'%'
 
 if __name__ == '__main__':
